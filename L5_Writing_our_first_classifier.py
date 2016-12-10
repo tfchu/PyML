41f29ddd3ec6d107a747cc4197af481784b8f5ad
test <test@test-VirtualBox>
2016-12-09 22:58:22 -0800

    added comments

--- L5_Writing_our_first_classifier.py
+++ L5_Writing_our_first_classifier.py
@@ -12,15 +12,27 @@ from sklearn.metrics import accuracy_score
 import random
 from scipy.spatial import distance
 
+#Euclidean formula c = sqrt(a^2 + b^2)
 def euc(a, b):
     return distance.euclidean(a, b)
 
 class ScrappyKNN():
+    #description
+    # input the training data
+    #param
+    # X_train: a set of training data (fields), e.g. [X1, X2, X3, ...] where X is a list of fields [x1, x2, x3, x4 ...]
+    # y_train: corresponding label for each training data (fields)
     def fit(self, X_train, y_train):
         self.X_train = X_train
         self.y_train = y_train
         #pass
-        
+    
+    #description
+    # get the predictions for each test data
+    #param
+    # X_test: a list of test data
+    #return
+    # a list of predicted lables
     def predict(self, X_test):
         predictions = []
         for row in X_test:
@@ -29,12 +41,25 @@ class ScrappyKNN():
             predictions.append(label)
         return predictions
         #pass
-        
+    
+    #description
+    # Used to determine which label in training data (y_train) a test data (one of X_test) belongs to
+    # by finding the shortest distance between the desired test data and training data (X_train), 
+    # the label of the closest training data (one of X_train) is then the predicted result (label)
+    # i.e. if the test data is closer to label A than to label B, then we say most likely the test data is label A (linear separation)
+    #param
+    # row: one test data, which is a list of fields
+    #return
+    # the predicted label of the test data
     def closest(self, row):
-        best_dist = euc(row, self.X_train[0])
+        #euc() finds the distance between: one set of test data, and one set of training data
+        #start with 1st training data, assume it is the best (shortest) distance
+        #then set the best index to index number of 1st training data, 0
+        best_dist = euc(row, self.X_train[0])   
         best_index = 0
+        #loop thru every training data set, and find the best (shortest) distance
         for i in range(1, len(self.X_train)):
-            dist = euc(row, self.X_train[i])
+            dist = euc(row, self.X_train[i])    
             if dist < best_dist:
                 best_dist = dist
                 best_index = i
@@ -82,6 +107,7 @@ if __name__ == '__main__':
     
     #take away:
     #K neighbor Classifier
+    #accuracy > 90% but is random, depending on what the training and test data are
     #pros: relatively simple
     #cons: computational intensive, hard to represent relationships between features
     
