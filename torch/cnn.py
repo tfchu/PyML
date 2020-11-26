af24e195233ec947e1382b10227e14dd4a2f8581
tfchu <tfchu@hotmail.com>
2020-11-25 16:08:12 -0800

    .

--- torch/cnn.py
+++ torch/cnn.py
@@ -33,9 +33,9 @@ transform = transforms.Compose(
 # DataLoader issue: cause multiprocess issue if num_workers=2 or above
 # - https://stackoverflow.com/questions/53974351/pytorch-getting-started-example-not-working
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=100, shuffle=True, num_workers=0)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True, num_workers=0)
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=0)
+testloader = torch.utils.data.DataLoader(testset, batch_size=32, shuffle=False, num_workers=0)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')              # 10 classes
 
