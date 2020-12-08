e33bdac5a77d549f2fb7d0a43455489dba29ebc2
tfchu <tfchu@hotmail.com>
2020-12-08 14:22:37 -0800

    .

--- torch/cnn_demo/cnn_net.py
+++ torch/cnn_demo/cnn_net.py
@@ -80,11 +80,9 @@ class Net(nn.Module):
         self.pool = nn.MaxPool2d(2, 2)
         self.conv3 = nn.Conv2d(32, 64, (3, 3))
         self.conv4 = nn.Conv2d(64, 64, (3, 3))
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv5 = nn.Conv2d(64, 128, (3, 3))
-        self.conv6 = nn.Conv2d(128, 128, (3, 3))        
-        self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(128 * 1 * 1, 128)
+        # self.conv5 = nn.Conv2d(64, 128, (3, 3))
+        # self.conv6 = nn.Conv2d(128, 128, (3, 3))        
+        self.fc1 = nn.Linear(64 * 5 * 5, 128)
         self.fc2 = nn.Linear(128, 10)
         # self.dropout1 = nn.Dropout(0.5)
         # self.dropout2 = nn.Dropout(0.2)
@@ -100,11 +98,11 @@ class Net(nn.Module):
         x = F.relu(self.conv4(x))       # [batch_size, 64, 10, 10]
         x = self.pool(x)                # [batch_size, 64, 5, 5]
 
-        x = F.relu(self.conv5(x))       # [batch_size, 128, 3, 3]
-        x = F.relu(self.conv6(x))       # [batch_size, 128, 1, 1]
-        x = self.pool(x)                # [batch_size, 128, 1, 1]
+        # x = F.relu(self.conv5(x))       # [batch_size, 128, 3, 3]
+        # x = F.relu(self.conv6(x))       # [batch_size, 128, 1, 1]
+        # x = self.pool(x)                # [batch_size, 128, 1, 1]
 
-        x = x.view(-1, 128 * 1 * 1)     # [batch_size, 128]
+        x = x.view(-1, 64 * 5 * 5)     # [batch_size, 128]
         x = self.fc1(x)                 # [batch_size, 128]
         x = F.relu(x)                   # [batch_size, 128]
         x = self.fc2(x)                 # [batch_size, 10]
